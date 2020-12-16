89e0cbe1f60f091fb07c1e48a78eee5f5dcb8730
xuwenlong <xuwenlong@deepsight.top>
2020-12-16 16:42:32 +0800

    1、增加移动功能

--- MyDeepZoomTest/MyDeepZoomTest/Src/MyDeepZoomTest/MainWindow.xaml.cs
+++ MyDeepZoomTest/MyDeepZoomTest/Src/MyDeepZoomTest/MainWindow.xaml.cs
@@ -21,24 +21,126 @@ namespace MyDeepZoomTest
     /// </summary>
     public partial class MainWindow : Window
     {
+        private System.Windows.Point lastMouseDownPos;
+        private System.Windows.Point lastMousePos;
+        private double MaxScaleZoom = 40.0;
+        public double curscale;
+        private bool isDrag;
         private MultiScaleImage msi = null;
 
-
+        /// <summary>
+        /// 无参构造
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
+            this.InitWindowSize();
             this.Loaded += MainWindow_Loaded;
         }
 
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            this.msi = new MultiScaleImage();
-            this.msi.Source = new MyMultiScaleTileSource(8448, 7936, 256, 0);
-            this.Bg.Children.Add(this.msi);
+            this.InitMultiScaleImage();
         }
 
-        private void MultiScaleImage_MouseWheel(object sender, MouseWheelEventArgs e)
+        /// <summary>
+        /// 初始化控件
+        /// </summary>
+        private void InitMultiScaleImage()
         {
+            this.msi = new MultiScaleImage();
+            //this.msi.Source = new MyMultiScaleTileSource(65792, 63488, 256, 0); // 9层图
+            this.msi.Source = new MyMultiScaleTileSource(8448, 7936, 256, 0); // 6层图
+            this.Bg.Children.Add(this.msi);
+
+            this.msi.MouseLeftButtonDown += Msi_MouseLeftButtonDown;
+            this.msi.MouseMove += Msi_MouseMove;
+            this.msi.MouseLeftButtonUp += Msi_MouseLeftButtonUp;
+            this.msi.MouseWheel += Msi_MouseWheel;
+            ZoomableCanvas.Refresh += ZoomableCanvas_Refresh;
+        }
+
+        private void Msi_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            this.isDrag = false;
+        }
+
+        private void Msi_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Keyboard.Focus(this);
+            this.lastMouseDownPos = e.GetPosition(msi);
+            this.isDrag = true;
+        }
+
+        private void Msi_MouseMove(object sender, MouseEventArgs e)
+        {
+            lastMousePos = e.GetPosition(msi);
+            if (isDrag)
+            {
+                double x = msi.ZoomableCanvas.Offset.X + (lastMouseDownPos.X - lastMousePos.X);
+                double y = msi.ZoomableCanvas.Offset.Y + (lastMouseDownPos.Y - lastMousePos.Y);
+                msi.ZoomableCanvas.Offset = new System.Windows.Point(x, y);
+                msi.ZoomableCanvas.ApplyAnimationClock(ZoomableCanvas.OffsetProperty, null);
+                lastMouseDownPos = lastMousePos;
+            }
+        }
+
+        private void ZoomableCanvas_Refresh(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            this.Refresh();
+        }
+
+        private void Refresh()
+        {
+            if (this.msi.ZoomableCanvas == null)
+            {
+                return;
+            }
+
+            this.curscale = msi.ZoomableCanvas.Scale * this.MaxScaleZoom;
+        }
+
+        /// <summary>
+        /// 鼠标滚轮事件
+        /// </summary>
+        private void Msi_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            System.Windows.Point position = e.GetPosition(this);
+            double tmpScale = this.curscale;
+            tmpScale = ((e.Delta <= 0) ? (tmpScale - 0.268745) : (tmpScale + 0.268745));
+            ZoomRatio(tmpScale, position.X, position.Y);
+        }
+
+        public void ZoomRatio(double zoom_ratio, double x, double y)
+        {
+            if(zoom_ratio < 0)
+            {
+                return;
+            }
+            System.Windows.Point point = new System.Windows.Point(x, y);
+            System.Windows.Point center = new System.Windows.Point(0.0, 0.0);
+            point = new System.Windows.Point(point.X - this.Bg.ActualWidth / 2.0, point.Y - this.Bg.ActualHeight / 2.0);
+            System.Windows.Point point2 = point;
+            double x2 = point2.X + msi.ActualWidth / 2.0;
+            double y2 = point2.Y + msi.ActualHeight / 2.0;
+            System.Windows.Point elementPoint = new System.Windows.Point(x2, y2);
+            System.Windows.Point point3 = msi.ElementToLogicalPoint(elementPoint);
+            msi.ZoomAboutLogicalPoint(zoom_ratio / curscale, point3.X, point3.Y);
+            curscale = zoom_ratio;
+        }
+
+        /// <summary>
+        /// 全屏，但现实任务栏
+        /// </summary>
+        private void InitWindowSize()
+        {
+            this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            this.WindowStyle = WindowStyle.None;
+            this.ResizeMode = ResizeMode.CanMinimize;
+            base.Top = 0;
+            base.Left = 0;
+            base.Width = SystemParameters.WorkArea.Width;
+            base.Height = SystemParameters.WorkArea.Height;
         }
     }
 }
